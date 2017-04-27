36a10ddaf87df9f883c2ca8ec813a0ec771b90de
Joel Ye <jye5@stuy.edu>
2017-04-27 14:51:59 -0400

    work

--- dec.py
+++ dec.py
@@ -6,7 +6,7 @@ def metaDecorator(function):
         # print '{0}({1}{2})'.format(function.func_name, *args, ','.join(**kwargs.value))
         return function(*args, **kwargs)
     return log_function
-
+"""
 def timer(f):
     def inner(*args, **kwargs):
         start_time = current_time()
@@ -14,12 +14,28 @@ def timer(f):
         end_time = current_time()
         print end_time - start_time
     return inner
-
+"""
 @metaDecorator
-@timer
 def fib(n):
     if n < 2:
         return n
     return fib(n - 1) + fib(n - 2)
 
 fib(5)
+
+def timer(f):
+    def inner(*arg):
+        start_time = current_time()
+        f(*arg)
+        end_time = current_time()
+        print end_time - start_time
+        return end_time - start_time
+    return inner
+
+@timer
+def slowFib(n):
+    if n < 2:
+        return n
+    return slowFib(n - 1) + slowFib(n - 2)
+
+slowFib(30)
